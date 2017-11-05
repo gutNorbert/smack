ac0d596cb5804b09c0d720a1541f2ef52be6c563
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-11-05 18:53:04 +0100

    LoginVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,20 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Gutpinter Norbert on 2017. 11. 05..
+//  Copyright © 2017. Gut Norbert. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Gutpinter Norbert on 2017. 11. 05..
+//  Copyright © 2017. Gut Norbert. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
