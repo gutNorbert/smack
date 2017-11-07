ec8bea450dfdb2c93093bd2fcd22c00e0baf3cb0
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-11-07 19:07:33 +0100

    web-request

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Succes: Bool) -> () //renaming a type typealias, webrequest succesful or not
+
+//URL Constants
+let BASE_URL = "https://chattychatke.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
+
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
