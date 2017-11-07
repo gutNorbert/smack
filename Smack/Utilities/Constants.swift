314786eb14939687321c4c0ce548222505c22ef4
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-11-07 20:19:59 +0100

    Logging a user

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -13,7 +13,7 @@ typealias CompletionHandler = (_ Succes: Bool) -> () //renaming a type typealias
 //URL Constants
 let BASE_URL = "https://chattychatke.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
-
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 //Segues
 
@@ -25,3 +25,8 @@ let UNWIND = "unwindToChannel"
 let TOKEN_KEY = "token"
 let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
+
+//Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
