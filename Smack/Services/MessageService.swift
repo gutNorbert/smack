f34f42275e27ff6d0f6603c5986b313b02670e32
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-11-16 18:24:51 +0100

    fetching messages

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -15,7 +15,8 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
-    var selectedChannel : Channel? 
+    var selectedChannel : Channel?
+    var messages = [Message]()
     
     func findAllChannel(completion: @escaping CompletionHandler) {
         Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
@@ -48,6 +49,39 @@ class MessageService {
                 debugPrint(response.result.error as Any)
             }
         }
+    }
+    
+    func findAllMessagesForChannel(channelID: String, completion: @escaping CompletionHandler){
+        Alamofire.request("\(URL_GET_MESSAGES)/\(channelID)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                self.clearMessages()
+                guard let data = response.data else {return}
+                if let json = JSON(data: data).array {
+                    for item in json{
+                        let messagebody = item["messageBody"].stringValue
+                        let channelId = item["channelID"].stringValue
+                        let id = item["_id"].stringValue
+                        let userName = item["userName"].stringValue
+                        let userAvatar = item["userAvatar"].stringValue
+                        let userAvatarColor = item["userAvatarColor"].stringValue
+                        let timeStamp = item["timeStamp"].stringValue
+                        
+                        let message = Message(message: messagebody, userName: userName, channelId: channelID, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+                        self.messages.append(message)
+                    }
+                    print(self.messages)
+                    completion(true)
+                }
+            } else {
+                debugPrint(response.result.error as Any)
+                completion(false)
+            }
+        }
+    }
+    
+    func clearMessages(){
+        messages.removeAll()
     }
     
     func clearChannels(){
