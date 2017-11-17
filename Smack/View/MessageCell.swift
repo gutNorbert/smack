00251577bb1614b12347bb5fbc9f303cc6104e6d
gutNorbert <32197748+gutNorbert@users.noreply.github.com>
2017-11-17 18:33:32 +0100

    timestamp

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -27,6 +27,21 @@ class MessageCell: UITableViewCell {
         userNameLbl.text = message.userName
         userImg.image = UIImage(named: message.userAvatar)
         userImg.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        guard var isoDate = message.timeStamp else {return}
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5) //cut the last 5 digit
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("z")) // puts z at the back
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a" // format month,day,hour,min,am/pm
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStampLbl.text = finalDate
+        }
     }
 
 }
